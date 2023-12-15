366f6e0193c81b2f2a6e22e34187f8ce3f3c8b1e
Nagendra Shiva <shivanagendragupta@gmail.com>
2023-12-14 20:12:01 -0500

    hhh

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
-from pydantic import BaseModel
-from typing import List
-from helpers import structured_generator
+class User(BaseModel):
+    name: str
+    age: int
+
+user = User(name="John Doe", age=30)
+
+print(user.__origin__)
+my_list = list(range(1, 11))
+from helpers import openai_model,prompt,custom_moel
 
 #Replace With Your Output
 class Titles(BaseModel):
     titles: List[str]
 
 #Replace with your input
-input = "Digital marketing"
+input = "Instagram marketing"
 
 #Replace with your prompt
-prompt = f"generate 5 blog titles for a video about {input}" 
+prompt = f"generate 5 titles for a video about {input}" 
 
 #Replace With Your Model
 openai_model = "gpt-3.5-turbo"
