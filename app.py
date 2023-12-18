8cb0d2e1c980f0441484dbec4796dee264751f89
Nagendra Shiva <shivanagendragupta@gmail.com>
2023-12-18 18:56:13 -0500

    a

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
-from typing import List
+from typing import List, Type
 from pydantic import BaseModel
-from helpers import structured_generator  # Corrected import statement
+import openai
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from the .env file
+load_dotenv()
+
+# Define the structured_generator function with correct typing
+def structured_generator(openai_model: str, prompt: str, custom_model: Type[BaseModel]) -> custom_model:
+    api_key = os.getenv("OPENAI_API_KEY")
+    openai.api_key = api_key
+
+    result = openai.Completion.create(
+        engine=openai_model, 
+        prompt=prompt,
+        max_tokens=100  # Adjust the max_tokens parameter as needed
+    )
+
+    # Assuming custom_model has a 'titles' attribute
+    titles_data = {'titles': [result['choices'][0]['text']]}
+    return custom_model(**titles_data)
 
 class User(BaseModel):
     name: str
@@ -8,7 +28,7 @@ class User(BaseModel):
 
 user = User(name="John Doe", age=30)
 
-print(user.__origin__)
+print(user.__class__.__name__)  # To print the class name
 my_list = list(range(1, 11))
 
 # Replace With Your Output
@@ -16,13 +36,13 @@ class Titles(BaseModel):
     titles: List[str]
 
 # Replace with your input
-input_text = "Instagram marketing"
+input_text = "Instagram caption"
 
 # Replace with your prompt
-prompt = f"generate 5 titles for a video about {input_text}"
+prompt = f"generate 5 titles for an Instagram post about {input_text}"
 
 # Replace With Your Model
-openai_model = "gpt-3.5-turbo"
+openai_model = "text-davinci-003"  # Use the appropriate GPT-3.5 model name
 
 result = structured_generator(openai_model, prompt, Titles)
 print(result.titles)
